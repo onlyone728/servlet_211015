7baac90056a0b5fdf1f026f7e02b7fcf1a8463c4
장하나 <onlyone728@gmail.com>
2022-01-12 20:53:21 +0900

    0112 과제

--- src/main/java/com/test/quiz/DatabaseQuiz01.java
+++ src/main/java/com/test/quiz/DatabaseQuiz01.java
@@ -19,11 +19,11 @@ public class DatabaseQuiz01 extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		response.setContentType("text/plain");
 		
-		// DB연결
+		// DB 연결
 		MysqlService mysqlService = MysqlService.getInstance();
 		mysqlService.connection();
 		
-		// insert
+		// insert query
 //		String insertQuery = "insert into `real_estate` (`realtorId`, `address`, `area`,`type`, `price`)"
 //				+ "values (3, '헤라펠리스 101동 5305호', 350, '매매', 1500000	)";
 //		try {
@@ -32,7 +32,8 @@ public class DatabaseQuiz01 extends HttpServlet {
 //			e.printStackTrace();
 //		}
 		
-		// select
+		// query 수행
+		// select query -> print
 		String selectQuery = "select * from `real_estate` order by `id` desc limit 10";
 		PrintWriter out = response.getWriter();
 		
@@ -40,11 +41,14 @@ public class DatabaseQuiz01 extends HttpServlet {
 			ResultSet resultSet = mysqlService.select(selectQuery);
 			
 			while (resultSet.next()) {
-				out.println("매물주소 : " + resultSet.getString("address") + " 면적 : " + resultSet.getInt("area")
-				+ " 타입 : " + resultSet.getString("type"));
+				out.println("매물주소 : " + resultSet.getString("address") + ", 면적 : " + resultSet.getInt("area")
+				+ ", 타입 : " + resultSet.getString("type"));
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
+		
+		// DB 연결 해제
+		mysqlService.disconnection();
 	}
 }
